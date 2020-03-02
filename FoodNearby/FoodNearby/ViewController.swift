48a5fd0c9f2a95297e5baa3f40a75a9260d8ca73
Rahul <rahul.dange1989@gmail.com>
2020-03-02 11:09:05 +0100

    Created XCode project.

---
+++ FoodNearby/FoodNearby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodNearby
+//
+//  Created by Rahul Dange on 02/03/20.
+//  Copyright © 2020 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
